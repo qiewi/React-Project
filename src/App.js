88d423e92f4bd359c2ebbfbbf73595ff15f5c3b6
qiewi <qiewifruit@gmail.com>
2024-08-30 23:58:46 +0700

    testing react features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = (props) => {
+  return (
+    <>
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h2>Age: {props.age}</h2>
+    </>
+    
+  )
+}
+
+const App = () => {
+  const isUserLoggedIn = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Person name={'Jane'} lastName={'Doe'} age = {'24'}/>
+        <Person name={'John'} lastName={'Doe'} age = {10+24}/>
     </div>
   );
 }
