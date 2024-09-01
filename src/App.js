163a6ec90c435a5007a72304d496c22e71d76d28
qiewi <qiewifruit@gmail.com>
2024-09-01 21:10:36 +0700

    finished search && useState

--- src/App.js
+++ src/App.js
@@ -9,16 +9,9 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=64b5dbca'
 
-const movie1 = {
-    "Title": "The Amazing Spiderman 2 Webb Cut",
-    "Year": "2021",
-    "imdbID": "tt18351128",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg"
-}
-
 const App = () => {
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -38,13 +31,13 @@ const App = () => {
             <div className="search">
                 <input 
                     placeholder="Search for movies"
-                    value="Superman"
-                    onChange = {() => {}}
+                    value={searchTerm}
+                    onChange = {(e) => setSearchTerm(e.target.value)}
                 />
                 <img 
                     src={SearchIcon}
                     alt="search"
-                    onClick = {() => {}}
+                    onClick = {() => searchMovies(searchTerm)}
                 />
             </div>
 
