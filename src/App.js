d3f676b18b27410091346d3c2df0cc0ba5c1b030
qiewi <qiewifruit@gmail.com>
2024-09-01 19:55:57 +0700

    use effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-    
-  )
-}
-
 const App = () => {
-  const isUserLoggedIn = true;
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(100);
+  }, [counter]);
 
   return (
     <div className="App">
-        <Person name={'Jane'} lastName={'Doe'} age = {'24'}/>
-        <Person name={'John'} lastName={'Doe'} age = {10+24}/>
+        <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+        <h1>{counter}</h1>
+        <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
